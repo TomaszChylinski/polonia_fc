8b25f12a06445c2cda644868ddc58b74a1d50f60
chylinski.t <chylinski.t@outlook.com>
2021-05-11 23:58:33 -0400

    added full schedule list

--- polonia-fc/src/app/components/schedule/schedule-item/schedule-item.component.ts
+++ polonia-fc/src/app/components/schedule/schedule-item/schedule-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-schedule-item',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schedule-item.component.scss']
 })
 export class ScheduleItemComponent implements OnInit {
+@Input() scheduleItems;
 
   constructor() { }
 
